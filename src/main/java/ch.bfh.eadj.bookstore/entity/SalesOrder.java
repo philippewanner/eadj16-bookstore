5740e425a3d6331dab0118daf61e798061232d4f
luthj7 <jan.luethi@students.bfh.ch>
2016-11-14 11:34:05 +0100

    added attributes to Entities

--- src/main/java/ch.bfh.eadj.bookstore/entity/SalesOrder.java
+++ src/main/java/ch.bfh.eadj.bookstore/entity/SalesOrder.java
@@ -1,5 +1,7 @@
 package ch.bfh.eadj.bookstore.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -12,6 +14,11 @@ public class SalesOrder {
     @Id
     private Long number;
     
+    private LocalDate date;
+    
+    private BigDecimal amount;
+    
+    private OrderStatus status;
     
     public enum OrderStatus {
         CANCELLED, ORDERED, PAYED, DELIVERED
--- src/main/java/ch.bfh.eadj.bookstore/entity/SalesOrderItem.java
+++ src/main/java/ch.bfh.eadj.bookstore/entity/SalesOrderItem.java
@@ -1,6 +1,8 @@
 package ch.bfh.eadj.bookstore.entity;
 
+import java.math.BigDecimal;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  * Created by philippewanner on 14.11.16.
@@ -8,4 +10,11 @@ import javax.persistence.Entity;
 
 @Entity
 public class SalesOrderItem {
+    
+    @Id
+    private Long id;
+    
+    private Integer quantity;
+    
+    private BigDecimal price;
 }
