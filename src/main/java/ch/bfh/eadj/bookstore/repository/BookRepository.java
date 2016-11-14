fa6c085b584bbd92dc2a4f54ea906d9d57cc07a5
lka <lka@ims.ch>
2016-11-14 14:34:56 +0100

    NamedQuery EM for Repository

--- src/main/java/ch/bfh/eadj/bookstore/repository/BookRepository.java
+++ src/main/java/ch/bfh/eadj/bookstore/repository/BookRepository.java
@@ -2,10 +2,15 @@ package ch.bfh.eadj.bookstore.repository;
 
 import ch.bfh.eadj.bookstore.entity.Book;
 
+import javax.persistence.EntityManager;
+
 /**
  * Jan
  */
 public class BookRepository extends AbstractRepository<Book, String> {
 
-    //search(String) List<BookInfo>
+	public BookRepository(EntityManager em) {
+		super(em);
+	}
+	//search(String) List<BookInfo>
 }
--- src/main/java/ch/bfh/eadj/bookstore/repository/CustomerRepository.java
+++ src/main/java/ch/bfh/eadj/bookstore/repository/CustomerRepository.java
@@ -2,11 +2,14 @@ package ch.bfh.eadj.bookstore.repository;
 
 import ch.bfh.eadj.bookstore.entity.Customer;
 
+import javax.persistence.EntityManager;
 
 /*
 Lukas
  */
 public class CustomerRepository extends AbstractRepository<Customer, Long> {
 
-
+	public CustomerRepository(EntityManager em) {
+		super(em);
+	}
 }
