daa46a958ca1b0c444e59e05787d5883941238a2
lka <lka@ims.ch>
2017-01-23 16:33:29 +0100

    ApplicationConfig für REST

---
+++ src/main/java/org/books/application/rest/ApplicationConfig.java
@@ -0,0 +1,8 @@
+package org.books.application.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class ApplicationConfig extends Application {
+}
