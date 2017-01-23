d719b45333b7b157241cf3fdae0fd9510f347550
lka <lka@ims.ch>
2017-01-23 17:00:48 +0100

    RegistrationResource

---
+++ src/main/java/org/books/application/rest/RegistrationResource.java
@@ -0,0 +1,72 @@
+package org.books.application.rest;
+
+import org.books.application.dto.Registration;
+import org.books.application.exception.CustomerAlreadyExistsException;
+import org.books.application.exception.CustomerNotFoundException;
+import org.books.application.exception.InvalidPasswordException;
+import org.books.application.service.CustomerService;
+import org.books.persistence.entity.Customer;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import static javax.ws.rs.core.MediaType.*;
+
+@Stateless
+@Path("registrations")
+public class RegistrationResource {
+
+	@EJB
+	private CustomerService service;
+
+	@POST
+	@Consumes({ APPLICATION_JSON, APPLICATION_XML })
+	@Produces(TEXT_PLAIN)
+	public Response registerCustomer(Registration registration) {
+		try {
+			long number = service.registerCustomer(registration);
+			return Response.status(Response.Status.CREATED).entity(String.valueOf(number)).build();
+		} catch (CustomerAlreadyExistsException e) {
+			throw new WebApplicationException("email address already used", Response.Status.CONFLICT);
+		}
+	}
+
+	@POST
+	@Produces(TEXT_PLAIN)
+	public Response authenticateCustomer(@HeaderParam("password") String password, @PathParam("email") String email) {
+		try {
+			Customer customer = service.findCustomer(email);
+			if (password == null) {
+				return Response.status(Response.Status.UNAUTHORIZED).build();
+			}
+
+			service.authenticateCustomer(email, password);
+
+			return Response.status(Response.Status.OK).entity(String.valueOf(customer.getNumber())).build();
+		} catch (CustomerNotFoundException e) {
+			throw new WebApplicationException("Customer not found", Response.Status.NOT_FOUND);
+		} catch (InvalidPasswordException e) {
+			throw new WebApplicationException(Response.Status.UNAUTHORIZED);
+		}
+	}
+
+	@PUT
+	@Consumes(TEXT_PLAIN)
+	@Produces(TEXT_PLAIN)
+	public Response changePassword(@PathParam("email") String email, String password) {
+		try {
+			Customer customer = service.findCustomer(email);
+			if (password == null) {
+				return Response.status(Response.Status.NO_CONTENT).build();
+			}
+
+			service.changePassword(email, password);
+
+			return Response.status(Response.Status.OK).entity(String.valueOf(customer.getNumber())).build();
+		} catch (CustomerNotFoundException e) {
+			throw new WebApplicationException("Customer not found", Response.Status.NOT_FOUND);
+		}
+	}
+}
