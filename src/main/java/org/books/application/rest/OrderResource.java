b4b8dbd5217e91168541f56b70e17999745ca8be
Philippe Wanner <keymaker00@tomylab.com>
2017-01-29 15:00:43 +0100

    remove unused imports and format text.

--- src/main/java/org/books/application/rest/OrderResource.java
+++ src/main/java/org/books/application/rest/OrderResource.java
@@ -4,25 +4,22 @@ import org.books.application.dto.PurchaseOrder;
 import org.books.application.dto.PurchaseOrderItem;
 import org.books.application.exception.*;
 import org.books.application.service.OrderService;
-import org.books.persistence.dto.BookInfo;
-
-import javax.ejb.EJB;
-import javax.ws.rs.*;
-import javax.ws.rs.core.Response;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import static javax.ws.rs.core.MediaType.APPLICATION_XML;
-import static org.books.application.rest.CatalogResource.logger;
 import org.books.persistence.dto.Order;
 import org.books.persistence.dto.OrderInfo;
 import org.books.persistence.dto.OrderItem;
 import org.books.persistence.dto.OrderRequest;
 import org.books.persistence.entity.SalesOrder;
 import org.books.persistence.entity.SalesOrderItem;
+
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_XML;
 
 /**
  * REST web service endpoint for orders resource
@@ -39,14 +36,25 @@ public class OrderResource {
         logger = Logger.getLogger(getClass().getName());
     }
 
-    /**
-     * Place Order ** Request HTTP Method POST Request Path
-     * /bookstore/rest/orders Headers Content-Type: {mime-type}, Content-Length:
-     * {number-of-bytes} Body order request data (see XML schema) Response
-     * Status Codes 201 Created 400 Bad Request (incomplete order data) 404 Not
-     * Found (customer or book not found) 402 Payment Required (payment error)
-     * Headers Content-Type: {mime-type}, Content-Length: {number-of-bytes} Body
-     * order data (see XML schema)
+    /** Place Order **
+     * --------------------------------------
+     * Request
+     * --------------------------------------
+     *  HTTP Method     POST
+     *  Request Path    /bookstore/rest/orders
+     *  Headers         Content-Type: {mime-type},
+     *                  Content-Length: {number-of-bytes}
+     *  Body            order request data (see XML schema)
+     * --------------------------------------
+     * Response
+     * --------------------------------------
+     *  Status Codes    201 Created
+     *                  400 Bad Request (incomplete order data)
+     *                  404 Not Found (customer or book not found)
+     *                  402 Payment Required (payment error)
+     *  Headers         Content-Type: {mime-type},
+     *                  Content-Length: {number-of-bytes}
+     *  Body            order data (see XML schema)
      */
     @POST
     @Consumes({APPLICATION_XML})
@@ -79,12 +87,20 @@ public class OrderResource {
         }
     }
 
-    /**
-     * Find Order by Number ** Request HTTP Method GET Request Path
-     * /bookstore/rest/orders/{number} Headers Accept: {mime-type} Body empty
-     * Response Status Codes 200 OK 404 Not Found (order not found) Headers
-     * Content-Type: {mime-type} Content-Length: {number-of-bytes} Body order
-     * data (see XML schema)
+    /** Find Order by Number **
+     * --------------------------------------
+     * Request
+     * --------------------------------------
+     *  HTTP Method      GET Request
+     *  Path            /bookstore/rest/orders/{number} Headers Accept: {mime-type} Body empty
+     * --------------------------------------
+     * Response
+     * --------------------------------------
+     *  Status Codes    200 OK
+     *                  404 Not Found (order not found)
+     *  Headers         Content-Type: {mime-type}
+     *                  Content-Length: {number-of-bytes}
+     *  Body            order data (see XML schema)
      */
     @GET
     @Path("{number}")
@@ -107,13 +123,22 @@ public class OrderResource {
         }
     }
 
-    /**
-     * Search Orders of Customer ** Request HTTP Method GET Request Path
-     * /bookstore/rest/orders?customerNr={number}&year={year} Headers Accept:
-     * {mime-type} Body empty Response Status Codes 200 OK 400 Bad Request
-     * (customer number or year missing) 404 Not Found (customer not found)
-     * Headers Content-Type: {mime-type} Content-Length: {number-of-bytes} Body
-     * list of order infos (see XML schema)
+    /** Search Orders of Customer **
+     * --------------------------------------
+     * Request
+     * --------------------------------------
+     *  HTTP Method     GET Request
+     *  Path            /bookstore/rest/orders?customerNr={number}&year={year}
+     *  Headers Accept: {mime-type}
+     *  Body            empty
+     * --------------------------------------
+     * Response
+     * --------------------------------------
+     *  Status Codes    200 OK 400 Bad Request (customer number or year missing)
+     *                  404 Not Found (customer not found)
+     *  Headers         Content-Type: {mime-type}
+     *                  Content-Length: {number-of-bytes}
+     *  Body            list of order infos (see XML schema)
      */
     @GET
     @Produces({APPLICATION_XML})
@@ -139,12 +164,23 @@ public class OrderResource {
         }
     }
 
-    /**
-     * Cancel Order ** Request HTTP Method DELETE Request Path
-     * /bookstore/rest/orders/{number} Headers none Body empty Remark The
-     * cancellation of an order causes a status change Response Status Codes 204
-     * No Content 403 Forbidden (order not cancelable) 404 Not Found (order not
-     * found) Headers none Body empty
+    /** Cancel Order **
+     * --------------------------------------
+     * Request
+     * --------------------------------------
+     *  HTTP Method     DELETE
+     *  Request Path    /bookstore/rest/orders/{number}
+     *  Headers         none
+     *  Body            empty
+     *  Remark          The cancellation of an order causes a status change
+     * --------------------------------------
+     * Response
+     * --------------------------------------
+     *  Status Codes    204 No Content
+     *                  403 Forbidden (order not cancelable)
+     *                  404 Not Found (order not found)
+     *  Headers         none
+     *  Body            empty
      */
     @DELETE
     @Path("{number}")
