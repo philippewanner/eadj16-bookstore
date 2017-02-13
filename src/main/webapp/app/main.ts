21f0a6a88ba1c47f5a1997ccd25461c81623ffc7
lka <lka@ims.ch>
2017-02-13 15:31:17 +0100

    authenticate Customer

--- src/main/webapp/app/main.ts
+++ src/main/webapp/app/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
-export const BOOKSTORE_REST_URL: string = "http://distsys.ch:8080/bookstore/rest";
+export const BOOKSTORE_REST_URL: string = "http://localhost:8080/bookstore/rest";
 
 platformBrowserDynamic().bootstrapModule(AppModule);
