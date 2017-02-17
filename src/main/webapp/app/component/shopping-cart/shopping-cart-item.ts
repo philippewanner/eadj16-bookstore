1e3c0db438ab468cbfdcb297f0eb1f933e5f7f5b
Philippe Wanner <keymaker00@tomylab.com>
2017-02-17 17:32:54 +0100

    Renaming book to bookInfo.

--- src/main/webapp/app/component/shopping-cart/shopping-cart-item.ts
+++ src/main/webapp/app/component/shopping-cart/shopping-cart-item.ts
@@ -1,10 +1,9 @@
-import {Book} from "../../core/book";
 import {BookInfo} from "../../core/book-info";
 
 export class ShoppingCartItem {
 
-    constructor(public book: BookInfo, public quantity: number) {}
+    constructor(public bookInfo: BookInfo, public quantity: number) {}
 
-    public getTotalPrice(): number {return this.quantity * this.book.price;}
+    public getTotalPrice(): number {return this.quantity * this.bookInfo.price;}
 }
 
